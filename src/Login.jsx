999a9e88b81d87f2de488eb1e33215c800e5a000
s569477 <s569477@nwmissouri.edu>
2025-02-11 15:11:38 -0600

    Placeholder Email
    Added a placeholder email to login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,5 @@
 import nwmsu_img_1 from './assets/northwestmissouri.jpg';
 import nwmsu_logo from './assets/nwmsu-logo.svg';
-// import api from './Config/axios';
 import {React, useState, useEffect} from 'react';
 
 export default function Login() {
@@ -41,6 +40,7 @@ export default function Login() {
                                                 id="email"
                                                 name="email"
                                                 type="email"
+                                                placeholder='Bearcat@nwmissouri.edu'
                                                 required
                                                 autoComplete="email"
                                                 className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-800 sm:text-sm sm:leading-6"
